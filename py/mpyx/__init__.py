5e9d544252fb8a97eb71894e74263b8e38048e3c
Martin Humphreys <n17r4m@gmail.com>
2018-06-06 17:27:53 -0600

    removed local mpyx

--- py/mpyx/__init__.py
+++
@@ -1,4 +0,0 @@
-from .F import EZ, As, F, By, Map, Seq, Sink
-from .F import Serial, Parallel, Broadcast, S, P, B
-from .F import Data
-from .util import Const, Iter, Print, Stamp, Read, Write, Filter, Batch, Zip
